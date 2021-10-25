b77331619af54546a9212481d911a07e349307b2
Csaba Fazakas <fazakas.cs@gmail.com>
2021-10-25 16:10:12 +0200

    Update ftp_upload_script.sh
    parameters instead of hardcoded values...

--- ftp_upload_script.sh
+++ ftp_upload_script.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 ##################################################################################
-# takes GNS*7002 files from <source folder>
+# takes input variables from 1st argument!!!
+# takes GNS*7002 files from /home_ext/amex_prod/outgoing/
 # checks if file was previously sent in uploaded_files.txt
 # if it was it moves the files to duplicate_attempts folder
 #       wasn't it sends the file and registers the file name in uploaded_files.txt
 ###################################################################################
+
+
 DATE="/bin/date"
-DIR="<source folder>"
-PATTERN="GNS\S+7002$"
-KEY="<path/rsa key>"
-DEST="<ip-address>"
-USERNAME="<ftp username>"
-DESTDIR="inbox"
-UPLOADED_FILES="uploaded_files.txt"
 
-# email variables
-email_to="<some email address>"
+echo `date +'%x %X'` INFO "Validating input arguments..."
+if [ -z "$1" ]
+    then
+    echo `date +'%x %X'` "WARNING You must provide 1st argument! Configuration file."
+    exit
+fi
 
-cd $DIR
+source $1
+
+
 if [ ! -f "./$UPLOADED_FILES" ]
 then
    touch "./$UPLOADED_FILES"
 fi
 
-number_of_files=`ls -l | egrep -E $PATTERN | wc -l`
+number_of_files=`ls -l | egrep -E "$PATTERN" | wc -l`
 echo `$DATE` "number of files:" $number_of_files
 
 if [ $number_of_files -eq 0 ]
@@ -32,7 +34,7 @@ then
    exit 1
 fi
 
-files_to_be_uploaded=`ls | egrep -E $PATTERN | awk 'BEGIN {FS=""} {ORS=" "}{print}'`
+files_to_be_uploaded=`ls | egrep -E "$PATTERN" | awk 'BEGIN {FS=""} {ORS=" "}{print}'`
 
 
 for file in $files_to_be_uploaded
@@ -41,17 +43,18 @@ do
    if [ $sent_already -eq 0 ]
    then
       file_size=`du -b $file | awk '{print $1}'`
-      if [ ! `expr $file_size % 1400` = 0 ]
+      if [ ! `expr $file_size % 1400` = 0 ] && [[ $1 == *"amex"* ]]
       then
          echo `$DATE` "ERROR Wrong file size"
-         mv $file ./files_with_wrong_size/
+         mv -f $file ./files_with_wrong_size/
          level="ERROR"
          result="FAILURE"
-         email_body="$file has size error (not divisible by 1400). Escalate to SVBO support team."
+         email_body="$email_client file $file has size error (not divisible by 1400). Escalate to SVBO support team."
       else
          echo `$DATE` "INFO Sending $file size: $file_size"
          sftp -o "IdentityFile=$KEY" $USERNAME@$DEST 2>&1 <<EOF
          cd $DESTDIR
+         mput $file
          ls -ltr
          quit
 EOF
@@ -62,12 +65,12 @@ EOF
             echo `$DATE` $file "Initial file sending..."  >> $UPLOADED_FILES
             level="INFO"
             result="SUCCESS"
-            email_body="$file was uploaded."
+            email_body="$file was uploaded to $email_client."
          else
             echo `$DATE` " ERROR upload failed."
             level="ERROR"
             result="FAILURE"
-            email_body="$file could not be uploaded due to transfer error. Wait for next execution. If it fails again, then escalate to Jenkins support team."
+            email_body="$file could not be uploaded to $email_client due to transfer error. Wait for next execution. If it fail again, then escalate to Jenkins support team."
          fi
       fi
    else
@@ -75,9 +78,9 @@ EOF
       mv -f $file ./duplicate_attempts/
       level="WARNING"
       result="FAILURE"
-      email_body="$file was already sent."
+      email_body="$file was already sent to $email_client."
    fi
-    echo "$email_body" | mail -v -s "$level File sending result: $result" $email_to
+    echo "$email_body" | mail -v -s "$level $email_client file sending result: $result" $email_to
 done
 
 
